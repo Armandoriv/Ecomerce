dcd7b02bce51b9987a507bb4e6e0cce5dd0365c4
unknown <armandoriv98@hotmail.com>
2022-12-02 16:41:30 -0600

    casi listo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,23 +1,22 @@
-import {useState, useEffect, useContext} from 'react';
+import {useState, useEffect} from 'react';
 import ItemList from '../itemList/itemList.jsx';
-import {consultarBDD} from '../../assets/funciones.js'
-import { useParams } from 'react-router-dom';
+import {getProductos} from '../../assets/firebase.js'
+import { useParams } from 'react-router-dom';;
 
 const ItemListContainer = () => {
 
     const [productos, setProductos] = useState([]);
     const {category} = useParams()
 
-
     useEffect(() => {
         if(category) {
-            consultarBDD('../json/productos.json').then(products => {
+            getProductos().then(products => {
                 const productsList= products.filter(prod => prod.idCategoria === parseInt(category))
                 const cardProductos = ItemList({productsList})
                 setProductos(cardProductos)
             })
         } else {
-            consultarBDD('./json/productos.json').then(productsList => {
+            getProductos().then(productsList => {
                 const cardProductos = ItemList({productsList})
                 setProductos(cardProductos)
             })
